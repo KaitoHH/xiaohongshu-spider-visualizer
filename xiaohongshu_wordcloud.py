a08d2f0c70bcf81154fcb039a3168a30b165fa29
KaitoHH <hh.kaito@gmail.com>
2018-07-27 22:05:51 +0800

    amazing word cloud

--- xiaohongshu_wordcloud.py
+++ xiaohongshu_wordcloud.py
@@ -1,6 +1,8 @@
 import csv
 
 import jieba.analyse
+import numpy as np
+from PIL import Image
 from wordcloud import WordCloud
 
 text = ''
@@ -9,6 +11,8 @@ with open('xiaohongshu.csv', encoding='utf-8') as file:
     for line in csv.reader(file):
         count += 1
         print(count)
+        # if count > 100:
+        #     break
         content = line[9]
         tags = jieba.analyse.extract_tags(content, topK=100, withWeight=False, allowPOS=('n'))
         text += ' '.join(tags) + ' '
@@ -18,8 +22,16 @@ with open('xiaohongshu.csv', encoding='utf-8') as file:
         #         text += word + ' '
 
 print(text)
-wordcloud = WordCloud(font_path='font/msyh.ttc', width=1920, height=1080).generate(text)
+
+mask = np.array(Image.open('mask.png'))
+
+wordcloud = WordCloud(background_color='white', mask=mask, font_path='font/msyh.ttc',
+                      color_func=lambda *args, **kwargs: (0xff, 0x28, 0x43),
+                      # contour_width=2,
+                      # contour_color='#ff8f8f',
+                      max_words=1000,
+                      scale=3).generate(text)
 
 image = wordcloud.to_image()
-image.save('wordcloud.png')
+wordcloud.to_file('wordcloud.png')
 image.show()
